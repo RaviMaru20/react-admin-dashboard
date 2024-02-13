4041bfc775e3263a6f1fb808919d0c6f922ae693
Ravi Local <ravilocal@mbp.local>
2024-02-13 18:09:39 -0500

    custom URL object for CI/CD

---
+++ client/src/assets/urlObj.js
@@ -0,0 +1,1 @@
+export const API_URL = 'https://journey-complexity-julia-prison.trycloudflare.com';
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_URL } from "assets/urlObj";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
